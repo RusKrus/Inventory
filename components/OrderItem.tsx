e1680c051b2b2c68ee04948cebbb4173630b6815
RusKrus <91795532+RusKrus@users.noreply.github.com>
2025-04-20 23:52:31 +0300

    hitfixOrderItem.tsx
    Удалил некорректную строку кода, которая ломала отображение количества продуктов в приходе

--- components/OrderItem.tsx
+++ components/OrderItem.tsx
@@ -54,7 +54,7 @@ export default function OrderItem({dispatch, order,  currencyState, isOpenedData
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={`size-10 border-1 border-black/10 rounded-full p-1 hover:cursor-pointer hover:fill-lime-500 hover:border-lime-500 duration-200 ${(isDetailsOpened&&openedOrderId===order.id)?'fill-lime-500 border-lime-500':''}`}>
                                 <path fillRule="evenodd" d="M2.625 6.75a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Zm4.875 0A.75.75 0 0 1 8.25 6h12a.75.75 0 0 1 0 1.5h-12a.75.75 0 0 1-.75-.75ZM2.625 12a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0ZM7.5 12a.75.75 0 0 1 .75-.75h12a.75.75 0 0 1 0 1.5h-12A.75.75 0 0 1 7.5 12Zm-4.875 5.25a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Zm4.875 0a.75.75 0 0 1 .75-.75h12a.75.75 0 0 1 0 1.5h-12a.75.75 0 0 1-.75-.75Z" clipRule="evenodd" />
                             </svg>
-                        </button>1
+                        </button>
                         <p>
                             <span className='font-semibold text-xl'>{numberOfProducts}</span> <br /> <span className='text-sm text-gray-400'>{productsWord}</span>
                         </p>
